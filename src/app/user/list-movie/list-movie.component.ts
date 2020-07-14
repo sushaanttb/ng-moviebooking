f30dc9ae4c0e914a3fee4790e50a731e1ae49c12
sushaanttb <sushantbhalla@ymail.com>
2020-07-14 15:01:38 +0530

    Viewing available Movie seats

--- src/app/user/list-movie/list-movie.component.ts
+++ src/app/user/list-movie/list-movie.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from 'src/app/movie.service';
 import { Location } from '@angular/common';
+import { Booking } from 'src/app/booking';
+
 import { MovieTheatre } from 'src/app/movie-theatre';
 
 
@@ -33,8 +35,22 @@ export class ListMovieComponent implements OnInit {
     let movieArray = [];
     let names: string[] = Object.keys(movieTheatre.movies);
     let slots: string[] = Object.values(movieTheatre.movies);
-    for (let i = 0; i < names.length; i++)
-      movieArray.push(names[i] + " (" + slots[i] + ") ");
+
+    let tmpBookings: Booking[] = [];
+    let availableMovieSeats: number = 0;
+
+    for (let i = 0; i < names.length; i++) {
+
+      availableMovieSeats = movieTheatre.capacity;
+      tmpBookings = movieTheatre.bookings[names[i]];
+
+      if (tmpBookings) {
+        tmpBookings.forEach((b: Booking) => availableMovieSeats -= b.numOfSeatsBooked);
+      }
+
+      movieArray.push(names[i] + " (" + slots[i] + ") " + " Available seats: " + availableMovieSeats);
+
+    }
     return movieArray;
   }
 
